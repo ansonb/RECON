7ee4b16e9becf6d269edbc910c1a11286287d992
anson <ansonbastos@gmail.com>
2022-07-02 16:04:17 +0530

    commit changes

--- GAT/utils.py
+++ GAT/utils.py
@@ -25,6 +25,7 @@ CUDA = torch.cuda.is_available()
 
 def save_model(model, name, epoch, folder_name):
     print("Saving Model")
+    os.makedirs(folder_name, exist_ok=True)
     torch.save(model.state_dict(),
                (folder_name + "trained_{}.pth").format(epoch))
     print("Done saving Model")
